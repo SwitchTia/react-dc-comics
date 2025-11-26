65669d2df987c0de08d9be711dd6da3e05744e58
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-26 21:43:22 +0100

    Feat: Comics section, with map

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,28 @@
 import { blueBgLinks } from "../assets/arraysLists/mainLists.js/blueBgLinks";
+import comicsList from "../assets/arraysLists/mainLists.js/comics";
 import style from "./Main.module.css";
 
 export default function Main() {
   return (
     <main>
-      <div className={`img-section py-40 text-center ${style.bgImg}`}>
-       
-      </div>
 
-      <div className={`py-40 text-center ${style.bgBlack}`}>
-        <h1>Content goes here</h1>
+      <div className={`img-section py-40 text-center ${style.bgImg}`}></div>
+
+
+
+      <div className={`comics d-flex wrap ${style.bgBlack}`}>
+
+        {comicsList.map((currentComics) => {
+            
+            return (
+              <div className={`col ${currentComics.type}`} key={currentComics.id}>
+
+                <img src={currentComics.thumb} alt={currentComics.title} />
+                <h5>{currentComics.title}</h5>
+                <p>Price: {currentComics.price}</p>
+              </div>
+            );
+          })}
       </div>
 
       <div className={`sectionBlue py-40 text-center ${style.bgBlue}`}>
@@ -17,7 +30,7 @@ export default function Main() {
 
           {blueBgLinks.map((currentLink, index) => (
             <li key={index}>
-
+            
               <a href={currentLink.path}>{currentLink.title}</a>
             </li>
 
