8358f19188f49241a35df4996c1f6c125286b34f
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-26 14:27:20 +0100

    Feat: Recreation of Header nav with Map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,41 +2,73 @@ import Logo from "./Logo";
 import style from "./Header.module.css";
 
 export default function Header() {
+
+const links = [
+    {
+      title: "Characters",
+      path: "/characters",
+      active: false,
+    },
+    {
+      title: "Comics",
+      path: "/comics",
+      active: true,
+    },
+    {
+      title: "Movies",
+      path: "/movies",
+      active: false,
+    },
+    {
+      title: "TV",
+      path: "/tv",
+      active: false,
+    },
+    {
+      title: "Games",
+      path: "/games",
+      active: false,
+    },
+    {
+      title: "Collectibles",
+      path: "/collectibles",
+      active: false,
+    },
+    {
+      title: "Videos",
+      path: "/videos",
+      active: false,
+    },
+    {
+      title: "Funs",
+      path: "/funs",
+      active: false,
+    },
+    {
+      title: "News",
+      path: "/news",
+      active: false,
+    },
+    {
+      title: "Shop",
+      path: "/shop",
+      active: false,
+    },
+    
+    
+  ];
+
   return (
     <header className="py-20 d-flex justify-content-between align-items-center">
       <Logo/>
       <nav className={style.nav}>
         <ul className="d-flex justify-content-center text-center g-20">
-          <li>
-            <a href="">Characters</a>
-          </li>
-          <li >
-            <a href="">Comics</a>
-          </li>
-          <li>
-            <a href="">Movies</a>
-          </li>
-          <li>
-            <a href="">TV</a>
-          </li>
-          <li className={style.active}>
-            <a href="">Games</a>
-          </li>
-          <li>
-            <a href="">Collectibles</a>
-          </li>
-          <li>
-            <a href="">Videos</a>
-          </li>
-          <li>
-            <a href="">Funs</a>
-          </li>
-          <li>
-            <a href="">News</a>
-          </li>
-          <li>
-            <a href="">Shop</a>
-          </li>
+          {links.map((currentLink, index) => (
+              <li key={index}>
+               <a href={currentLink.path}>{currentLink.title}</a>
+              </li>
+
+          ))}
 
         </ul>
       </nav>
